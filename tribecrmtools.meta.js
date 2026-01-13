fa142f435ce47e198a694037f7580e36bdeb3ecf
DanielOndiordna <80363737+DanielOndiordna@users.noreply.github.com>
2026-01-13 10:33:46 +0100

    Create tribecrmtools.meta.js

---
+++ tribecrmtools.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Tribe CRM tools
+// @namespace    https://gesp.zn-man.nl/
+// @updateURL    https://raw.githubusercontent.com/DanielOndiordna/Tribe/master/dist/tribecrmtools.meta.js
+// @downloadURL  https://raw.githubusercontent.com/DanielOndiordna/Tribe/master/dist/tribecrmtools.user.js
+// @version      2026.1.13.2
+// @description  Dankzij deze plugin zijn er diverse tools om Tribe een beetje beter te maken. De instellingen en keuzes voor deze tools worden alleen opgeslagen in deze browser sessie en worden niet bewaard in Tribe.
+// @author       Daniel
+// @match        https://app.tribecrm.nl/*
+// @match        https://auth.tribecrm.nl/login*
+// @match        https://gesp.zn-man.nl/tools/plugins
+// @grant        none
+// ==/UserScript==
